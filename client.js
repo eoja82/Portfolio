928cde4b74889ce2123e19f32995080d2538a378
Erik Oja <erik_oja@yahoo.com>
2020-03-02 19:11:15 -0600

    add skills and overlay on projects style

--- client.js
+++ client.js
@@ -1,36 +1,83 @@
-const imgs = [
-  {src: "img/IMG_0887.jpeg", alt: "bear"},
-  {src: "img/IMG_0926.jpeg", alt: "cat and dog"},
-  {src: "img/IMG_0928.jpeg", alt: "cat and dog"},
-  {src: "img/IMG_0933.jpeg", alt: "cat and dog"},
-  {src: "img/IMG_0983.jpg", alt: "cat and dog"},
-  {src: "img/IMG_1003.jpg", alt: "bear"},
-  {src: "img/IMG_1004.jpg", alt: "bear"},
-  {src: "img/IMG_5949.jpg", alt: "cat and dog"}
+const portfolio = [
+  {src: "img/IMG_0887.jpeg", alt: "bear", skills: ["HTML", "Javascript", "CSS", "React"]},
+  {src: "img/IMG_0926.jpeg", alt: "cat and dog", skills: ["bootstrap", "Javascript", "CSS"]},
+  {src: "img/IMG_0928.jpeg", alt: "cat and dog", skills: ["HTML", "node.js", "d3.js"]},
+  {src: "img/IMG_0933.jpeg", alt: "cat and dog", skills: ["python", "C", "npm"]},
+  {src: "img/IMG_0983.jpg", alt: "cat and dog", skills: ["npm", "bootstrap", "Javascript"]},
+  {src: "img/IMG_1003.jpg", alt: "bear", skills: ["node.js", "npm", "bootstrap"]},
+  {src: "img/IMG_1004.jpg", alt: "bear", skills: ["mongoDB", "mongoose", "express"]},
+  {src: "img/IMG_5949.jpg", alt: "cat and dog", skills: ["HTML", "React", "gatsby", "Javascript"]}
 ];
 
-// add images to DOM
-const images = document.querySelector(".images");
+const skillset = ["All Skills", "Javascript", "React", "CSS",
+                "python", "C", "npm", "node.js", "mongoDB", "mongoose", "express", "HTML", "gatsby"];
 
-imgs.forEach( (x, i) => {
+// add portfolio to DOM
+const projects = document.querySelector("#projects");
+
+portfolio.forEach( (x, i) => {
   // div wrapper parent element to apply css perspective
   let div = document.createElement("div");
-  div.className = "photoDiv";
+  div.className = "projectDiv projectActive";
   let img = document.createElement("img");
-  img.className = "photoNotFlipped"
+  img.className = "project";
+  img.style.visibility = "hidden";
   img.id = `photo${i}`
   img.src = x.src;
   img.alt = x.alt;
   div.appendChild(img);
-  images.appendChild(div);
+  projects.appendChild(div);
+  // overlay on hover
+  let overlay = document.createElement("div");
+  overlay.className = "overlay";
+  let overlayText = document.createElement("div");
+  overlayText.className = "overlayText";
+  x.skills.forEach( skill => {
+    let p = document.createElement("p");
+    p.innerText = skill;
+    overlayText.appendChild(p);
+  });
+  overlay.appendChild(overlayText);
+  div.appendChild(overlay);
 });
+
+// add skills to DOM
+const skills = document.querySelector("#skills");
+
+skillset.forEach( (x, i) => {
+  let button = document.createElement("button");
+  button.className = "skillBtn";
+  button.innerText = x;
+  button.id = `skill${i}`;
+  button.addEventListener("click", filter);
+  skills.appendChild(button);
+});
+
+function filter(event) {
+  let projects = document.querySelectorAll(".projectDiv");
+  let buttons = document.querySelectorAll(".skillBtn");
+  projects.forEach( (x, i) => {
+    if (portfolio[i].skills.includes(event.target.innerText) || event == "All Skills") {
+      x.classList.add("projectActive");
+    } else {
+      x.classList.remove("projectActive");
+    }
+  });
+  buttons.forEach( x => {
+    if (event.target.id === x.id) {
+      x.classList.add("activeBtn");
+    } else {
+      x.classList.remove("activeBtn");
+    }
+  });
+}
 
 // fade in and flip images on scroll
 (function() {
   let images, windowHeight;
 
   function init() {
-    images = document.querySelectorAll(".photoNotFlipped");
+    images = document.querySelectorAll(".project");
     windowHeight = window.innerHeight;
     //console.log("windowHeight: " + windowHeight);
   }
@@ -39,13 +86,15 @@ imgs.forEach( (x, i) => {
     let i = 0;
     for (i; i < images.length; i++) {
       let positionFromTop = images[i].getBoundingClientRect().top;
-      if (positionFromTop + 75 - windowHeight <= 0) {
-        images[i].className = "photoFlipped"
+      if (positionFromTop + 5 - windowHeight <= 0) {
+        images[i].classList.add("animated", "slideInUp")
+        images[i].style.visibility = "visible";
       } else {
-        images[i].className = "photoNotFlipped";
+        images[i].className = "project";
+        images[i].style.visibility = "hidden";
       }
     }
-  }
+  } 
 
   window.addEventListener("scroll", imgPosition);
   window.addEventListener("resize", init);
