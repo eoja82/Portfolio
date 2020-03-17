88b30988696a3e424d6dd64b8eb4d12d2f2f1774
Erik Oja <erik_oja@yahoo.com>
2020-03-17 18:35:31 -0500

    add project data

--- public/data.js
+++ public/data.js
@@ -1,15 +1,73 @@
 const portfolio = [
-  {src: "./public/img/IMG_0887.jpeg", alt: "bear", skills: ["HTML", "Javascript", "CSS", "React"]},
-  {src: "./public/img/IMG_0926.jpeg", alt: "cat and dog", skills: ["bootstrap", "Javascript", "CSS"]},
-  {src: "./public/img/IMG_0928.jpeg", alt: "cat and dog", skills: ["HTML", "node.js", "d3.js"]},
-  {src: "./public/img/IMG_0933.jpeg", alt: "cat and dog", skills: ["python", "C", "npm"]},
-  {src: "./public/img/IMG_0983.jpg", alt: "cat and dog", skills: ["npm", "bootstrap", "Javascript"]},
-  {src: "./public/img/IMG_1003.jpg", alt: "bear", skills: ["node.js", "npm", "bootstrap"]},
-  {src: "./public/img/IMG_1004.jpg", alt: "bear", skills: ["mongoDB", "mongoose", "express"]},
-  {src: "./public/img/IMG_5949.jpg", alt: "cat and dog", skills: ["HTML", "React", "gatsby", "Javascript"]}
+  {
+    src: "./public/img/mineSweeper.png", 
+    alt: "Minesweeper Game", 
+    view: "https://codepen.io/EOJA/full/zYYmgpe",
+    code: "https://github.com/eoja82/Mine-Sweeper",
+    about: "The classic computer game built with vanilla Javascript, HTML, and CSS",
+    skills: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    src: "./public/img/stockPriceChecker.png", 
+    alt: "Stock Price Checker", 
+    view: "https://fccstockpricecheck.glitch.me/",
+    code: "https://glitch.com/edit/#!/fccstockpricecheck?path=routes/api.js:6:1",
+    about: "A full stack app built with JavaScript, Node.js, Mongoose, Express and MongoDB that gets current stock prices from an API.  The app also includes Chai assertion testing and Helmet information security.",
+    skills: ["JavaScript", "HTML", "CSS", "Mongoose", "Node.js", "Express", "Chai", "Helmet", "MongoDB"]
+  },
+  {
+    src: "./public/img/issueTracker.png", 
+    alt: "Issue Tracker", 
+    view: "https://fcc-track-issues.glitch.me/",
+    code: "https://glitch.com/edit/#!/fcc-track-issues?path=routes/api.js:1:0",
+    about: "A full stack app built with JavaScript, Node.js, Mongoose, Express and MongoDB that can track issues for projects.  The app also includes Chai assertion testing.",
+    skills: ["JavaScript", "HTML", "CSS", "Mongoose", "Node.js", "Express", "Chai", "Helmet", "MongoDB"]
+  },
+  {
+    src: "./public/img/chatRoom.png", 
+    alt: "Chat Room", 
+    view: "https://rapid-powder.glitch.me/",
+    code: "https://glitch.com/edit/#!/rapid-powder?path=server.js:1:0",
+    about: "A full stack Socket.IO chat app.  Login with GitHub authentication strategy for Passport.",
+    skills: ["JavaScript", "HTML", "CSS", "Node.js", "Express", "MongoDB", "Socket.IO", "Passport"]
+  },
+  {
+    src: "./public/img/choroplethMap.png", 
+    alt: "US Education Choropleth Map", 
+    view: "https://codepen.io/EOJA/full/OePXNb",
+    code: "https://github.com/eoja82/fcc-Data-Visualization-Projects/tree/master/ChoroplethMap",
+    about: 'A D3.js data visualization project.  Follow the "View" link to see more D3.js projects.',
+    skills: ["JavaScript", "HTML", "CSS", "D3.js"]
+  },
+  {
+    src: "./public/img/pomodoroClock.png", 
+    alt: "Pomodoro Clock", 
+    view: "https://eoja82-pomodoro-clock.glitch.me/",
+    code: "https://github.com/eoja82/Pomodoro-Clock",
+    about: "Built with React, Node.js, and Express.",
+    skills: ["React", "JavaScript", "Node.js", "HTML", "CSS"]
+  },
+  {
+    src: "./public/img/calculator.png", 
+    alt: "Calculator", 
+    view: "https://eoja82-calculator.glitch.me/",
+    code: "https://github.com/eoja82/Calculator",
+    about: "Built with React, Bootstrap, Node.js, and Express.",
+    skills: ["Node.js", "React", "JavaScript", "Bootstrap", "HTML", "CSS"]
+  },
+  {
+    src: "./public/img/randomQuotes.png", 
+    alt: "Random Quote Generator", 
+    view: "https://eoja82-random-quote-generator-1.glitch.me/",
+    code: "https://github.com/eoja82/Random-Quote-Generator",
+    about: "Built with React, Node.js, and Express.",
+    skills: ["Node.js", "React", "JavaScript", "Bootstrap", "HTML", "CSS"]
+  }
 ];
 
-const skillset = ["All Skills", "Javascript", "React", "CSS",
-                "python", "C", "npm", "node.js", "mongoDB", "mongoose", "express", "HTML", "gatsby"];
+const skillset = [
+  "JavaScript", "React", "HTML", "CSS", "Node.js", "MongoDB", "Mongoose", "Express", "Python", "C", "SQL"
+];
+
 
 export { portfolio, skillset }
