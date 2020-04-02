53b81a753428002eeaca4e3575d4eaac32fcb923
Erik Oja <erik_oja@yahoo.com>
2020-04-02 16:01:38 -0500

    add helmet

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const app = express();
+
+app.use(helmet());
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
