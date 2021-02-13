d9f93405c97c533f79009e9ab01f540722541183
Erik Oja <erik_oja@yahoo.com>
2021-02-13 16:43:38 -0600

    Updates

--- smoothScroll.js
+++ smoothScroll.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $("a[href^='#']").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== "" && navigator.userAgent.indexOf("Chrome") === -1) {
       // Prevent default anchor click behavior
       event.preventDefault();
 
